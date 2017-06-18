b9b86d1c0181e6ee4687c2cc0a440bfeac013e20
chqngh-berkeley <chqngh@berkeley.edu>
2017-06-18 03:34:39 -0700

    created env shell script

---
+++ create_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERNAME=$1
+ENV_NAME=$2
+
+echo "username: $USERNAME"
+echo "creating env = $ENV_NAME"
+conda create -n $ENV_NAME --copy python=2.7 jupyter pandas
+source activate $ENV_NAME
+conda install --copy -c conda-forge mrjob=0.5.5
+cd .conda/envs/$ENV_NAME/
+zip -r ~/$ENV_NAME.zip *
+cd ~
+hdfs dfs -mkdir /user/$USERNAME/virtualenv/
+hdfs dfs -put $ENV_NAME.zip /user/$USERNAME/virtualenv/
+# jupyter notebook
+# !python mrjob_systems_test.py stuff.txt -r hadoop \
+#     --python-bin=./my_env/bin/python \
+#     --archive=”hdfs:///user/USERNAME/virtualenv/my_env.zip#my_env”
